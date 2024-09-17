47f064fa801da005e6eb86caf38289c877cc5dfd
Fiona Baci <fionabaci04@gmail.com>
2024-09-17 04:06:11 -0400

    had merge issues - resolved

--- src/render-functions.js
+++ src/render-functions.js
@@ -1,21 +1,15 @@
 export const renderBookList = (bookListEl, books) => {
-    // take in a container and dta and render into a container
-    /* FEEDBACK:
-     * in the next line you are trying to set the innerHTML of the bookListEl to an empty string but you are using the subtraction operator instead of the correct one which is the assignment operator.
-     */
-    bookListEl.innterHTML - '';
+    // take in a container and data and render into a container
+    bookListEl.innterHTML = '';
     books.forEach((book) => {
         const li = document.createElement('li');
         const img = document.createElement('img');
         const pTag = document.createElement('p');
-        const button = document.createEleemt('button')// this is a typo
-    })
-/* FEEDBACK:
- * So here you have the right idea of creating the elements for each book but you are not appending correctly.
-the code below should be inside the forEach loop and you should append the elements to the li element, then append the li element to the bookListEl.
- */
-// append
-li.append(img, p, button)
+        const button = document.createElement('button')
+        
+        img.src = book.coverUrl;
+        img.alt = `An old cover of ${book.title}`
+li.append(img, pTag, button)
 bookListEl.append(li)
     })
 
@@ -23,24 +17,87 @@ bookListEl.append(li)
 }
 
 export const renderAuthorInfo = (authorInfoEl, author) => {
-    /* FEEDBACK:
-     * This will be similar to the renderBookList function but you will be rendering the author info instead of the book info.
-     */
+    authorInfoEl.innterHTML = '';
+        const h2 = document.createElement('h2')
+        h2.context = author.name
+        
+        const img = document.createElement('img');
+        img.src = author.pictureUrl
+        img.alt = `A picture of ${author.name}`
+    
+        const p = document.createElement('p');
+        p.textContent = `Born: ${author.birthDate}`
+    
+        const pBio= document.createElement('p');
+        pBio.textContent = author.bio
+        
+        const a = document.createElement('a');
+        a.href = author.wikipediaUrl;
+        a.textContent = 'Wikipedia Link';
+
+        authorInfoEl.append(h2, img, p, pBio, a);
 }
 
 export const renderNewUserForm = (newUserFormEl) => {
-    /* FEEDBACK:
-     * This function will be responsible for rendering the form to create a new user.
-    just build out a form with the necessary fields and append it to the newUserFormEl.
-    read the requirements in the README to see what fields are required.
-    follow the pattern of building out a form
-    <form>
-        <label for="">Username</label>
-        <input type="text" id="username">
-     */
-}
+    newUserFormEl.innerHTML = '';
+
+    const usernameLabel = document.createElement('label');
+    usernameLabel.textContent = 'Username';
+    usernameLabel.setAttribute('for', 'username');
+
+    const userInput = document.createElement('input');
+    userInput.id = 'username';
+    userInput.name = 'username';
+
+    const coolLabel = document.createElement('label');
+    coolLabel.textContent = 'Is this user cool?';
+    coolLabel.setAttribute('for', 'is-cool');
+
+    const cool = document.createElement('input');
+    cool.id = 'is-cool';
+    cool.name = 'isCool';
+    cool.type = 'checkbox';
+
+
+    const favLang = document.createElement('label');
+    favLang.textContent = 'Favorite Language';
+    favLang.setAttribute('for', 'favorite-language');
+
+    const selectFavLang = document.createElement('select');
+    selectFavLang.id = 'favorite-language';
+    selectFavLang.name = 'favoriteLanguage';
+
+    const diffLanguages = ['None', 'JavaScript', 'Python', 'Ruby'];
+    diffLanguages.forEach(language => {
+        const option = document.createElement('option');
+        option.value = language;
+        option.textContent = language;
+        selectFavLang.append(option);
+    });
+
+
+    const createUserButton = document.createElement('button');
+    createUserButton.textContent = 'Create User';
+
+
+    newUserFormEl.append(usernameLabel, userInput, coolLabel, cool, favLang, selectFavLang, createUserButton);
+};
+
+    
 
 export const renderNewUser = (newUserEl, newUser) => {
-}
+newUserEl.innerHTML = "";
 
-fb
+    const h2 = document.createElement("h2");
+    h2.textContent = newUser.username;
+    h2.dataset.userId = newUser.id;
+
+    const pTag = document.createElement("p");
+    if (newUser.isCool === true) pTag.textContent = `The hippest in the house!`;
+    else pTag.textContent = "A real square.";
+
+    const pTag2 = document.createElement("p");
+    pTag2.textContent = `Favorite Language: ${newUser.favoriteLanguage}`;
+
+    newUserEl.append(h2, pTag, pTag2)
+};
